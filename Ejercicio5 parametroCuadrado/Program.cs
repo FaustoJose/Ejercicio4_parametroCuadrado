ec4beca5f5e5fe5e3d64c4ab4eff79674c2d5058
Fausto Jose Tineo Mejia <2016-2155@est.itsc.edu.do>
2021-08-08 20:23:16 -0400

    Agregar archivos de proyecto.

---
+++ Ejercicio5 parametroCuadrado/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Ejercicio5_parametroCuadrado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cuadrado(10,10);
+        }
+        public static void Cuadrado(int altura, int anchura)
+        {
+            for (int i=0;i<=(altura-1);i++) {
+                for (int f = 0; f <= (anchura - 1); f++)
+                {
+                    Console.Write("*");
+                    Console.Write(" ");
+                }
+
+                Console.WriteLine("");
+            }
+        }
+    }
+}
